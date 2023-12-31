0be8acecde23b58de8326aca94845600c470ad60
Janelaxagh <hell.agbsus@gmail.com>
2023-12-31 21:01:28 +0600

    action for createCourseAction

---
+++ src/features/courses-list/action.ts
@@ -0,0 +1,9 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { coursesRepository } from './courses-repository'
+
+export const createCourseAction = async (command: CreateCourseListElementCommand, revalidatePagePath: string) => {
+  await coursesRepository.createCourseElement(command)
+  revalidatePath(revalidatePagePath)
+}
