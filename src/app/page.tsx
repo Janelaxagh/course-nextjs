481b4f9c13c23aa7264c032e6096c05fd62cc3a3
Janelaxagh <hell.agbsus@gmail.com>
2023-12-31 21:00:58 +0600

    import this pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from '@/shared/ui/button'
+import { CoursesList } from '@/features/courses-list/pub/courses-list'
+import { CreateCourseForm } from '@/features/courses-list/pub/create-course-form'
 
-export default function Home() {
+export default async function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Button>Button</Button>
+    <main className='flex min-h-screen flex-col p-8'>
+      <CreateCourseForm revalidatePagePath='/' className='w-[300px] mb-10' />
+      <CoursesList revalidatePagePath='/' />
     </main>
   )
 }
