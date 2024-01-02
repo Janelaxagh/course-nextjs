8b1f2b2ab682ce3ad7c0db88b8ebe3e47b248de9
Janelaxgh <janelaxagh.ant@gmail.com>
2024-01-02 23:45:31 +0600

    change test

--- src/features/courses-list/ui/course-item.spec.tsx
+++ src/features/courses-list/ui/course-item.spec.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { CourseItem } from './course-item'
 
-describe('course item', () => {
+describe('courses item', () => {
   it('should call delete callback', async () => {
     const onDelete = jest.fn()
     render(
