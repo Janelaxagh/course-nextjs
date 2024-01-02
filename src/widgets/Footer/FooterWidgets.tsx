b925eec7f236b8fb6de97baa30bf4c2e7207fe00
Janelaxgh <janelaxagh.ant@gmail.com>
2024-01-02 23:45:03 +0600

    FooterWidgets

---
+++ src/widgets/Footer/FooterWidgets.tsx
@@ -0,0 +1,9 @@
+import { FooterEntities } from '@/entities/footer'
+
+export const FooterWidgets = () => {
+  return (
+    <footer className='bg-gray-800 text-gray-300 py-8'>
+      <FooterEntities />
+    </footer>
+  )
+}
